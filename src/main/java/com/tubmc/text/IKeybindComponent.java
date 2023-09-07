769b69191dccd1432dc4a82fe3a0b3112304b2b9
BradBot_1 <BradBot_1@outlook.com>
2023-09-07 02:03:05 +0100

    Keybind component

---
+++ src/main/java/com/tubmc/text/IKeybindComponent.java
@@ -0,0 +1,24 @@
+package com.tubmc.text;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import com.tubmc.text.builder.KeybindComponentBuilder;
+
+import fun.bb1.objects.annotations.DisallowsEmptyString;
+
+public non-sealed interface IKeybindComponent extends IComponent {
+	
+	public static @NotNull IKeybindComponent of(final @NotNull @DisallowsEmptyString String keybind) {
+		return IMPLEMENTATION.createKeybind(keybind);
+	}
+	
+	public static @NotNull KeybindComponentBuilder builder(@NotNull final String keybind) {
+		return new KeybindComponentBuilder(keybind);
+	}
+	
+	public @Nullable @DisallowsEmptyString String getKeybind();
+	
+	public void setKeybind(final @Nullable @DisallowsEmptyString String newKeybind);
+	
+}
