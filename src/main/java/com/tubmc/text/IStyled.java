aca1b8fbffa965a1f11d007e656e6eeef399723f
BradBot_1 <BradBot_1@outlook.com>
2023-09-11 00:26:41 +0100

    IStyled JavaDoc

--- src/main/java/com/tubmc/text/IStyled.java
+++ src/main/java/com/tubmc/text/IStyled.java
@@ -32,37 +32,55 @@ import fun.bb1.objects.annotations.AllowsEmptyString;
  */
 @Internal
 sealed interface IStyled permits IComponent {
-	
+	/**
+	 * @since 1.0.0
+	 * @return The {@link Color} to display as
+	 */
 	public @Nullable Color getColor();
-	
+	/**
+	 * @since 1.0.0
+	 * @param newColor The {@link Color} to display as
+	 */
 	public void setColor(final @Nullable Color newColor);
 	/**
 	 * @since 1.0.0
 	 * @return If this text will display as bold
 	 */
 	public boolean isBold();
-	
+	/**
+	 * @since 1.0.0
+	 * @param isBold If the element should be bold
+	 */
 	public void setBold(final boolean isBold);
 	/**
 	 * @since 1.0.0
 	 * @return If this text will display as italicised
 	 */
 	public boolean isItalic();
-	
+	/**
+	 * @since 1.0.0
+	 * @param isItalic If the element should be italicised
+	 */
 	public void setItalic(final boolean isItalic);
 	/**
 	 * @since 1.0.0
 	 * @return If this text will display as underlined
 	 */
 	public boolean isUnderlined();
-	
+	/**
+	 * @since 1.0.0
+	 * @param isUnderlined If the element should be underlined
+	 */
 	public void setUnderlined(final boolean isUnderlined);
 	/**
 	 * @since 1.0.0
 	 * @return If this text will display with a line through it
 	 */
 	public boolean isStrikedThrough();
-	
+	/**
+	 * @since 1.0.0
+	 * @param isStrikedThrough If the element should be stricken through
+	 */
 	public void setStrikeThrough(final boolean isStrikedThrough);
 	/**
 	 * @since 1.0.0
@@ -70,13 +88,19 @@ sealed interface IStyled permits IComponent {
 	 * @see <a href="https://www.minecraftforum.net/forums/minecraft-java-edition/redstone-discussion-and/commands-command-blocks-and/2766692-obfuscation-in-entity-names#c6">https://www.minecraftforum.net/forums/minecraft-java-edition/redstone-discussion-and/commands-command-blocks-and/2766692-obfuscation-in-entity-names#c6</a>
 	 */
 	public boolean isObfuscated();
-	
+	/**
+	 * @since 1.0.0
+	 * @param isObfuscated If the element should be obfuscated
+	 */
 	public void setObfuscated(final boolean isObfuscated);
 	/**
 	 * @since 1.0.0
 	 * @return The font for this {@link IComponent} (or null if using the default one)
 	 */
 	public @Nullable @AllowsEmptyString String getFont();
-	
+	/**
+	 * @since 1.0.0
+	 * @param newFont The font to display under
+	 */
 	public void setFont(final @Nullable @AllowsEmptyString String newFont);
 }
