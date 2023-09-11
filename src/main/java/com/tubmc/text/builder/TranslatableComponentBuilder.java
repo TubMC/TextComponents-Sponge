976a49776064d8dcbc7c5e04bd2c60962ac75f79
BradBot_1 <BradBot_1@outlook.com>
2023-09-11 01:34:02 +0100

    Start TranslatableComponentBuilder JavaDoc

--- src/main/java/com/tubmc/text/builder/TranslatableComponentBuilder.java
+++ src/main/java/com/tubmc/text/builder/TranslatableComponentBuilder.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.function.Consumer;
 
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.ApiStatus.Internal;
 
 import com.tubmc.text.IComponent;
 import com.tubmc.text.ILiteralComponent;
@@ -16,12 +16,59 @@ import fun.bb1.objects.annotations.AllowsEmptyString;
 import fun.bb1.objects.annotations.DisallowsEmptyString;
 import fun.bb1.objects.defineables.ITypedBuilder;
 
+/**
+ *    Copyright 2023 TubMC.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+/**
+ * An {@link AbstractComponentBuilder} for building {@link ITranslatableComponent}
+ * 
+ * @author BradBot_1
+ * @since 1.0.0
+ * @version 1.0.0
+ * @see AbstractComponentBuilder
+ * @see ITranslatableComponent
+ */
 public final class TranslatableComponentBuilder extends AbstractComponentBuilder<TranslatableComponentBuilder> {
-	
+	/**
+	 * The translation to lookup
+	 * 
+	 * @since 1.0.0
+	 * @see ITranslatableComponent#of(String)
+	 */
 	private final @NotNull @DisallowsEmptyString String translation;
-	private @Nullable @AllowsEmptyString String fallback;
+	/**
+	 * The selector to lookup
+	 * 
+	 * @since 1.0.0
+	 * @see ITranslatableComponent#of(String, String)
+	 * @see ITranslatableComponent#of(String, String, Collection)
+	 */
+	private @Nullable @AllowsEmptyString String fallback = null;
+	/**
+	 * The selector to lookup
+	 * 
+	 * @since 1.0.0
+	 * @see ITranslatableComponent#of(String, Collection)
+	 * @see ITranslatableComponent#of(String, String, Collection)
+	 */
 	private final @NotNull Collection<@NotNull IComponent> inserts = new ArrayList<@NotNull IComponent>();
-	
+	/**
+	 * @param translation The translationKey to use
+	 * @since 1.0.0
+	 * @see ITranslatableComponent#builder(String)
+	 */
 	@Internal
 	public TranslatableComponentBuilder(@NotNull final String translation) {
 		this.translation = translation;
@@ -81,17 +128,26 @@ public final class TranslatableComponentBuilder extends AbstractComponentBuilder
 		consumer.accept(builder);
 		return this;
 	}
-	
+	/**
+	 * Clears all inserts
+	 * 
+	 * @since 1.0.0
+	 * @return The builder instance
+	 */
 	public final @NotNull TranslatableComponentBuilder noInserts() {
 		this.inserts.clear();
 		return this;
 	}
-	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected final @NotNull TranslatableComponentBuilder self() {
 		return this;
 	}
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public final @NotNull ITranslatableComponent build() {
 		if (this.inserts.size() == 0) {
@@ -105,5 +161,4 @@ public final class TranslatableComponentBuilder extends AbstractComponentBuilder
 		}
 		return this.applyCommon(ITranslatableComponent.of(this.translation, this.fallback, this.inserts));
 	}
-	
 }
