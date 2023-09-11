6c6b35ee8398e3ccdbcf13268a5336dbfe8d4e72
BradBot_1 <BradBot_1@outlook.com>
2023-09-11 01:02:09 +0100

    Builder Style JavaDoc

--- src/main/java/com/tubmc/text/builder/Style.java
+++ src/main/java/com/tubmc/text/builder/Style.java
@@ -1,10 +1,60 @@
 package com.tubmc.text.builder;
 
+import com.tubmc.text.IComponent;
+
+/**
+ *    Copyright 2023 TubMC.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+/**
+ * Styles that can be set in {@link IComponent}
+ * 
+ * @author BradBot_1
+ * @since 1.0.0
+ * @version 1.0.0
+ * @see AbstractComponentBuilder
+ */
 public enum Style {
+	/**
+	 * A bold element
+	 * 
+	 * @since 1.0.0
+	 */
 	BOLD,
+	/**
+	 * An italicised element
+	 * 
+	 * @since 1.0.0
+	 */
 	ITALIC,
+	/**
+	 * An underlined element
+	 * 
+	 * @since 1.0.0
+	 */
 	UNDERLINE,
+	/**
+	 * A stricken element
+	 * 
+	 * @since 1.0.0
+	 */
 	STRIKETHOUGH,
+	/**
+	 * An obfuscated element
+	 * 
+	 * @since 1.0.0
+	 */
 	OBFUSCATED,
 	;
 }
